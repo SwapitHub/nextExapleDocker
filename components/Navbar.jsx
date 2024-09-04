4198abf94ac629eaff6e7e9baaba0c67da2a4e47
shamsher singh <shamshersingh8574@gmail.com>
2024-09-04 14:44:32 +0530

    commit message

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,20 @@
 import Link from "next/link";
+import { HomePage } from "./homePages/home/page";
 
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center bg-purple-600 px-8 py-3">
-      <Link className="text-white font-bold" href={"/"}>
-        Integration Ninjas
-      </Link>
-      <Link href={"/addTopic"}>
-      <button className="bg-white text-purple-600 font-bold py-2 px-4 rounded-md hover:bg-purple-100">
+        <Link href={"/addTopic"}>
+        <button className="bg-white text-purple-600 font-bold py-2 px-4 rounded-md hover:bg-purple-100">
           Add Topic
       </button>
       </Link>
+      
+      <Link className="text-white font-bold" href="/">
+        Integration Ninjas
+        <HomePage/>
+      </Link>
+    
     </nav>
   );
 }
